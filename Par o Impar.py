d45bc73727e8e03e977ad6946892408ba48e63f5
Federico <federico.g.ezequiel@gmail.com>
2023-08-26 20:36:11 -0300

    Script en python para saber el numero es par o impar

---
+++ Par o Impar.py
@@ -0,0 +1,7 @@
+#Algoritmo para determinar si un numero es par o impar
+
+n = int(input("Ingrese un numero "))
+if n % 2 == 0:
+    print("El numero",n," es par")
+else: 
+    print("El numero",n," es impar")
